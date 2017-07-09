c52d589f9dc2bd61bfaa1c3fd33ac8a6e70d8074
chenzc <dazzworld@163.com>
2017-07-09 19:08:41 +0800

    update NubiaCamera

--- camera/Android.mk
+++ camera/Android.mk
@@ -10,32 +10,4 @@ LOCAL_SRC_FILES     := NubiaCamera.apk
 LOCAL_CERTIFICATE := platform
 LOCAL_PRIVILEGED_MODULE := true
 
-#PRODUCT_COPY_FILES += \
-#    $(call find-copy-subdir-files,*.so,$(LOCAL_PATH)/lib/arm64-v8a,$(TARGET_OUT_APPS)/$(LOCAL_MODULE)/lib/arm64)
-
-copy_files := $(subst $(LOCAL_PATH)/,, \
-	$(filter-out %.mk,\
-	$(patsubst ./%,%, \
-	$(shell find $(LOCAL_PATH)/lib/arm64-v8a -type f -name "*" -and -not -name ".*"  -exec basename {} \; | sort) \
-	)))
-
-AV_LIBS := libnubia_delay_recorder.so
-AV_SYMLINKS := $(addprefix $(TARGET_OUT_APPS_PRIVILEGED)/$(LOCAL_MODULE)/lib/arm64/,$(notdir $(AV_LIBS)))
-
-$(AV_SYMLINKS):
-	@mkdir -p $(dir $@)
-	ln -sf /system/lib64/$(notdir $@) $@
-
-AV_LIBS := libnubia_delay_recorder.so
-
-LOCAL_ADDITIONAL_DEPENDENCIES := $(AV_SYMLINKS)
-
-$(foreach file,$(copy_files),\
-$(shell cp $(LOCAL_PATH)/lib/arm64-v8a/$(file) $(TARGET_OUT_APPS_PRIVILEGED)/$(LOCAL_MODULE)/lib/arm64/$(file)))
-
 include $(BUILD_PREBUILT)
-
-
-
-#PRODUCT_COPY_FILES += $(foreach file,$(copy_files),\
-#    $(LOCAL_PATH)/$(file):$(TARGET_OUT_APPS)/$(LOCAL_MODULE)/lib/arm64/$(file))
